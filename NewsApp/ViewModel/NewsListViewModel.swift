a6eba8bf8be4b565a9408de18b6791fd9a1450f5
seherkose <seherkose1@outlook.com>
2023-12-04 17:41:44 +0300

    unchanged file

--- NewsApp/ViewModel/NewsListViewModel.swift
+++ NewsApp/ViewModel/NewsListViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+protocol NewListViewModel: AnyObject{
+    func showLoadingView()
+    func dismissLoadingView()
+    func showBadStuffAlert(title: String, message:String, buttonTitle:String)
+    func updateData()
+    
+}
+class NewsListViewModel {
+    
+    weak var delegate: NewListViewModel?
+    
+    init(){}
+    
+    private var hasMoreNews = true
+    private var articleData: [Article] = []
+    
+    
+    func getNews(countryName: String, page: Int) {
+        //showLoadingView()
+        if !hasMoreNews {
+            return
+        }
+        delegate?.showLoadingView()
+        
+        NetworkManager.shared.getNews(for: countryName, page: page) { [weak self] result in
+            // for unwrapping optional self result of weak
+            guard let self = self else {
+                return
+            }
+            //self.dismissLoadingView()
+            delegate?.dismissLoadingView()
+            
+            switch result {
+            case .success(let news):
+                if news.articles.count < 10 {
+                    self.hasMoreNews = false
+                }
+                self.articleData = news.articles
+//                let uniqueArticles = Array(Set(news.articles))
+//                self.article.append(contentsOf: uniqueArticles)
+                //self.updateData(on: self.article)
+                delegate?.updateData()
+                
+                
+            case .failure(let error):
+                //                    self.presentNAAlertOnMainThread(title: Constants.NewsListVC.badStuff, message: error.rawValue, buttonTitle: Constants.NewsListVC.okMessage)
+                delegate?.showBadStuffAlert(title: Constants.NewsListVC.badStuff, message: error.rawValue, buttonTitle: Constants.NewsListVC.okMessage)
+                delegate?.dismissLoadingView()
+                
+            }
+        }
+    }
+}
